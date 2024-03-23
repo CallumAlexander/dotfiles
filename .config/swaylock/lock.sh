f4307130e12bd25505b836c0a8377a2831602c98
Callum Alexander <callumalexander.personal@gmail.com>
2024-03-23 12:38:51 +0000

    Added swaylock config

---
+++ .config/swaylock/lock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+swaylock --clock \
+         --screenshots \
+         --indicator \
+         --indicator-caps-lock \
+         --show-keyboard-layout \
+         --font "Lato" \
+         --fade-in 0.5 \
+         --effect-blur=3x3 \
+         --effect-vignette=0.3:0.9 \
+         --inside-ver-color "#61AFEF" \
+         --inside-wrong-color "#E06C75"
