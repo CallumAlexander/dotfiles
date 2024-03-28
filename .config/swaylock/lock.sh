1dc9eea3ed0c11ddf23f43c5352acd952ec5a34e
Callum Alexander <callumalexander.personal@gmail.com>
2024-03-28 15:50:16 +0000

    Updates
    Changed some minor things about hypr including border width because it wasn't clear enough for me.
    Swaylock blur needed to be much much stronger because you could literally read what my screen said haha.
    Some minor waybar css edits to make it look nice.

--- .config/swaylock/lock.sh
+++ .config/swaylock/lock.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 swaylock --clock \
-         --screenshots \
-         --indicator \
-         --indicator-caps-lock \
-         --show-keyboard-layout \
-         --font "Lato" \
-         --fade-in 0.5 \
-         --effect-blur=3x3 \
-         --effect-vignette=0.3:0.9 \
-         --inside-ver-color "#61AFEF" \
-         --inside-wrong-color "#E06C75"
+	--screenshots \
+	--indicator \
+	--indicator-caps-lock \
+	--show-keyboard-layout \
+	--font "Lato" \
+	--fade-in 1 \
+	--effect-blur=4x4 \
+	--effect-vignette=0.3:0.9 \
+	--inside-ver-color "#61AFEF" \
+	--inside-wrong-color "#E06C75"
+
